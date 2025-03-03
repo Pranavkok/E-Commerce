962c2eeffbf258f38e47d94b0bfd24d19eaa4e29
Pranav Kokate <pranav@Pranavs-MacBook-Air.local>
2025-03-03 18:34:58 +0530

    Enhance Header and Search components for mobile responsiveness

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,34 +1,67 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import Search from './Search'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { FaRegUserCircle } from "react-icons/fa";
+import useMobile from '../hooks/useMobile';
+import { FaShoppingCart } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const [isMobile] = useMobile()
+  const location = useLocation()
+  const isSearchPage = location.pathname === '/search'
+  const navigate = useNavigate()
+
+  const redirectToLogin = ()=>{
+      navigate("/login")
+  }
+
   return (
-    <div>
-      <header className='h-20 shadow-md sticky top-0'>
-        <div className='container mx-auto items-center h-full flex px-2 justify-between'>
+      <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 flex items-center justify-center flex-col gap-1'>
+            {
+                !(isMobile && isSearchPage)&&(<div className='container mx-auto items-center flex px-2 justify-between'>
 
-          {/* logo section */}
-          <div className='h-full'>
-            <Link to={"/"} className='h-full flex justify-center items-center'>
-              <img src={logo} alt="logo" width={170} height={60} className='hidden lg:block'/>
-              <img src={logo} alt="logo" width={170} height={60} className='lg:hidden'/>
-            </Link>
-          </div>
-
-          {/* search */}
-          <div>
-            <Search/>
-          </div>
-
-          {/* login and my cart */}
-          <div>
-            login cart 
-          </div>
+                {/* logo section */}
+                <div className='h-full'>
+                  <Link to={"/"} className='h-full flex justify-center items-center'>
+                    <img src={logo} alt="logo" width={170} height={60} className='hidden lg:block'/>
+                    <img src={logo} alt="logo" width={170} height={60} className='lg:hidden'/>
+                  </Link>
+                </div>
+      
+                {/* search */}
+                <div className='hidden lg:block'>
+                  <Search/>
+                </div>
+      
+                {/* login and my cart */}
+                <div>
+                  {/* for the mobile */}
+                  <button className='text-neutral-600 lg:hidden'>
+                    <FaRegUserCircle size={27} />
+                  </button>
+                  {/* for the Desktop */}
+                  <div className='hidden lg:flex items-center gap-10'>
+                    <div onClick={redirectToLogin} className='text-lg px-2'>Login</div>
+                    <button className='flex items-center gap-3 bg-secondary-200 hover:bg-green-700 px-3 py-3 rounded text-white'>
+                      {/* add to cart icon */}
+                      <div className='animate-bounce'>
+                          <FaShoppingCart size={26} />
+                      </div>
+                      <div className='font-semibold'>
+                          <p>My Cart </p>
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              </div>)
+            }
+        
+        <div className='container mx-auto px-2 lg:hidden'>
+          <Search/>
         </div>
       </header>
-    </div>
   )
 }
 
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { FaSearch } from "react-icons/fa";
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { TypeAnimation } from 'react-type-animation';
+import { IoMdArrowRoundBack } from "react-icons/io";
+import useMobile from '../hooks/useMobile';
 
 const Search = () => {
 
     const navigate = useNavigate()
     const location = useLocation()
     const [isSearchPage,setSearchPage] = useState(false)
+    const [isMobile] = useMobile()
+
 
     useEffect(()=>{
         const isSearch = location.pathname === "/search" ;
@@ -21,16 +25,28 @@ const Search = () => {
     }
 
   return (
-    <div className='w-full min-w-[300px] lg:min-w-[420px] h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50'>
-        <button className='flex justify-center items-center h-full p-3'>  
-            <FaSearch size={22} />
-        </button>
-
+    <div className='w-full min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200'>
+        <div>           
+            {
+                (isMobile && isSearchPage)?
+                (
+                <Link to={"/"} className='flex justify-center items-center h-full p-1 m-1 group-focus-within:text-primary-200 bg-white rounded-full shadow-md'>
+                    <IoMdArrowRoundBack size={22}/>
+                </Link>
+                )
+                :
+                (
+                <button className='flex justify-center items-center h-full p-3 group-focus-within:text-primary-200'>  
+                    <FaSearch size={22} />
+                </button>
+                )
+            }           
+        </div>
         <div>
             {
                 (!isSearchPage)?(
                     // Not inside the search Page
-                    <div onClick={redirectoSearchPage}>
+                    <div onClick={redirectoSearchPage} className='w-full h-full flex items-center'>
                         <TypeAnimation
                         sequence={[
                             'Search "milk"', 
@@ -63,7 +79,13 @@ const Search = () => {
                 ):(
                      // Inside the search Page 
                     <div>
-                        <input type="text" className='' placeholder='Search for Atta, Dal and more.' />
+                        <input type='text'
+                            placeholder='Search for atta dal and more.'
+                            autoFocus
+                            // defaultValue={searchText}
+                            className='bg-transparent w-full h-full outline-none'
+                            // onChange={handleOnChange} 
+                            />
                     </div>
                 )
             }
